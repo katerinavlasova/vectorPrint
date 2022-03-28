ff57bed28ca709ff64ddd74935df0dc9454cad0d
katerinavlasova <derkamesser@yandex.ru>
2022-03-28 09:33:46 +0300

    base vector class, two and three dimensional vector classes

--- vectorsPrint/vectorsPrint.cpp
+++ vectorsPrint/vectorsPrint.cpp
@@ -5,6 +5,50 @@
 
 using namespace std;
 
+class baseVector {
+public:
+    baseVector() {};
+    ~baseVector() {};
+    virtual double length() { return 0;  };
+};
+
+class twoDimensionalVector : baseVector {
+private:
+    double x1;
+    double x2;
+public:
+    twoDimensionalVector(double value1, double value2) 
+    { 
+        x1 = value1;
+        x2 = value2;
+    };
+    ~twoDimensionalVector() {};
+    virtual double length() override
+    {
+        return sqrt((pow(x1, 2) + pow(x2, 2)));
+    };
+};
+
+class threeDimensionalVector : baseVector {
+private:
+    double x1;
+    double x2;
+    double x3;
+public:
+    threeDimensionalVector(double value1, double value2, double value3)
+    {
+        x1 = value1;
+        x2 = value2;
+        x3 = value3;
+    }
+
+    ~threeDimensionalVector() {};
+
+    virtual double length() override
+    {
+        return sqrt((pow(x1, 2) + pow(x2, 2) + pow(x3, 2)));
+    };
+};
 
 int main()
 {
