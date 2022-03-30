8f28ed1b3abc3cca0ecf9c20491197c9a0e3265f
katerinavlasova <derkamesser@yandex.ru>
2022-03-30 13:01:37 +0300

    cleaned main function

--- vectorsPrint/vectorsPrint.cpp
+++ vectorsPrint/vectorsPrint.cpp
@@ -13,46 +13,17 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    ///list newnode = list();
     twoDimensionalVector vec(3, 4);
-    list newnode = list(&vec);
-    cout << endl;
-    cout << endl;
-    newnode.printLengths();
     twoDimensionalVector vec2(1, 1);
     threeDimensionalVector vec3(1, 1, 2);
 
+    list newnode = list(&vec);
+    
+    newnode.printLengths();
+    
     newnode.addVector(&vec2);
     newnode.addVector(&vec3);
-    cout << endl;
-    cout << endl;
-    newnode.printLengths();
 
-    cout << endl;
-    cout << endl;
     newnode.printLengths();
-    //newnode.addVector(vec);
-//    newnode.addNode(&vec2);
-//    newnode.addNode(&vec3);
-    cout << endl;
-    cout << endl;
-    cout << endl;
-    newnode.printLengths();
-    //newnode.printNodes();
-    //newnode.~list();
-    //_CrtDumpMemoryLeaks();
-    //cout << newnode->getCurrentVector()->length();
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
